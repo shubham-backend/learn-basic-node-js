2a423bc4af9ed27bc5864ebf3f6e44b110cfa1e7
shubham <shubham.gupta1@appinventiv.com>
2020-03-12 13:00:36 +0530

    .env setup and MySql Database Connectivity

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var http = require('http');
 
-//Creating a server
-var server = http.createServer(function(req, res){
+var express = require('express');
+var app = express();
+var routes = require("./routes/v1/api.js");
 
-    res.writeHead(200, {'contet-type': 'text/plain'});
-    res.end('Node js Started');
-});
-server.listen(3000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:3000/ ');
+
+app.listen(3000, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:3000/');
+
+//Creating a server
+// var server = http.createServer(function(req, res){
+
+//     res.writeHead(200, {'contet-type': 'text/plain'});
+//     res.end('Node js Started');
+// });
+// server.listen(3000, '127.0.0.1');
+// console.log('Server running at http://127.0.0.1:3000/ ');
